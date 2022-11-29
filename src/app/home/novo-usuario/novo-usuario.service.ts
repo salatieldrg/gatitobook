f6cab019d7a85fbc23806ed8ec3409f21aaf80e7
Salatiel dos Reis <salatieldrg@gmail.com>
2022-11-29 10:45:09 -0300

    Criação da tela de cadastro de usuários com formulário reativo

---
+++ src/app/home/novo-usuario/novo-usuario.service.ts
@@ -0,0 +1,16 @@
+import { NovoUsuario } from './novo-usuario';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NovoUsuarioService {
+
+  constructor(private http: HttpClient) { }
+
+  cadastrarNovoUsuario(novoUsuario: NovoUsuario): Observable<NovoUsuario>{
+    return this.http.post<NovoUsuario>('http://localhost:3000/user/signup', novoUsuario);
+  }
+}
