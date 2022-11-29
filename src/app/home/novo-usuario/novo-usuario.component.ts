9b09d6745e39a66925f4e49726363dc9fe9f5ea8
Salatiel dos Reis <salatieldrg@gmail.com>
2022-11-29 11:59:21 -0300

    Validações customizadas

--- src/app/home/novo-usuario/novo-usuario.component.ts
+++ src/app/home/novo-usuario/novo-usuario.component.ts
@@ -2,6 +2,7 @@ import { NovoUsuarioService } from './novo-usuario.service';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NovoUsuario } from './novo-usuario';
+import { minusculoValidatror } from './minusculo.validator';
 
 @Component({
   selector: 'app-novo-usuario',
@@ -16,7 +17,7 @@ export class NovoUsuarioComponent implements OnInit{
 
   ngOnInit(): void {
     this.novoUsuarioForm = this.formBuilder.group({
-      userName: [''],
+      userName: ['', minusculoValidatror],
       email: ['', [Validators.required, Validators.email]],
       fullName: ['', [Validators.required, Validators.minLength(4)]],
       password: [''],
