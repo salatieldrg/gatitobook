a5af48c6c9dd12543d0c99dc4d79208b4fd193f2
Salatiel dos Reis <salatieldrg@gmail.com>
2022-11-29 11:45:10 -0300

    Validação Padrão na tela de cadastro de usuários

--- src/app/home/novo-usuario/novo-usuario.component.ts
+++ src/app/home/novo-usuario/novo-usuario.component.ts
@@ -1,6 +1,6 @@
 import { NovoUsuarioService } from './novo-usuario.service';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NovoUsuario } from './novo-usuario';
 
 @Component({
@@ -17,8 +17,8 @@ export class NovoUsuarioComponent implements OnInit{
   ngOnInit(): void {
     this.novoUsuarioForm = this.formBuilder.group({
       userName: [''],
-      email: [''],
-      fullName: [''],
+      email: ['', [Validators.required, Validators.email]],
+      fullName: ['', [Validators.required, Validators.minLength(4)]],
       password: [''],
     });
   }
