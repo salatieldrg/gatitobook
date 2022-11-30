f377019d8a8a007c1bc70479a6e43592f3b6815a
Salatiel dos Reis <salatieldrg@gmail.com>
2022-11-29 23:29:36 -0300

    Criação do módulo de cartão

---
+++ src/app/componentes/cartao/cartao.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-cartao',
+  templateUrl: './cartao.component.html',
+  styleUrls: ['./cartao.component.css']
+})
+export class CartaoComponent {
+  @Input()
+  titulo = '';
+
+}
