c36fc68ee272cc82dd1d11dc3ad0368ed729e3ba
Salatiel dos Reis <salatieldrg@gmail.com>
2022-12-10 12:30:44 -0300

    Criação da tela de listagem de animais

---
+++ src/app/animais/grade-fotos-animais/grade-fotos-animais.component.ts
@@ -0,0 +1,13 @@
+import { Animais } from './../animal';
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-grade-fotos-animais',
+  templateUrl: './grade-fotos-animais.component.html',
+  styleUrls: ['./grade-fotos-animais.component.css']
+})
+export class GradeFotosAnimaisComponent {
+  @Input()
+  animais!: Animais;
+
+}
--- src/app/animais/lista-animais/lista-animais.component.ts
+++ src/app/animais/lista-animais/lista-animais.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { AnimaisService } from './../animais.service';
+import { UsuarioService } from './../../autenticacao/usuario/usuario.service';
+import { Animais } from './../animal';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-lista-animais',
   templateUrl: './lista-animais.component.html',
   styleUrls: ['./lista-animais.component.css']
 })
-export class ListaAnimaisComponent {
+export class ListaAnimaisComponent implements OnInit{
+  animais!: Animais;
+
+  constructor(
+    private usuarioService: UsuarioService,
+    private animaisService: AnimaisService
+  ){}
+
+  ngOnInit(): void {
+    this.usuarioService.retornaUsuario().subscribe(
+      (usuario) => {
+        const nomeUsuario = usuario.name ?? '';
+        this.animaisService.listaDoUsuario(nomeUsuario).subscribe(
+          (animais) => {
+            this.animais =  animais;
+          }
+        );
+      }
+    );
+  }
+
 
 }
